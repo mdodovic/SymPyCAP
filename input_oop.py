c024e1dd62bc7303206a1e4a019fc149ee152bed
KatStan <43248875+KatStan@users.noreply.github.com>
2020-11-23 01:37:55 +0100

    Add files via upload

---
+++ input_oop.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 23 00:25:17 2020
+
+@author: Katarina
+"""
+from symPyCAP_oop import Solution
+
+elements = [
+            ["R", "R1", 2, 1],
+            ["R", "R2", 1, 0],
+            ["R", "R3", 1, 0],
+            ["V", "Ug", 2, 0]          
+            ]
+
+rab = Solution()
+print(rab.symPyCAP(elements))
+
