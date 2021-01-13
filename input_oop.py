01353604ffd088fbedd3cbb09430b64530d9e9ca
mdodovic <32821985+mdodovic@users.noreply.github.com>
2021-01-13 18:49:14 +0100

    everything fixed

--- input_oop.py
+++ input_oop.py
@@ -1,35 +1,61 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Nov 23 00:25:17 2020
-@authors: 
-    Katarina Stanković (sk180183d@student.etf.bg.ac.rs)
-    Matija Dodović (dm180072d@student.etf.bg.ac.rs)
+
+@author: Katarina
 """
 from symPyCAP_oop import Solution
-from sympy import symbols
+import sympy
+import math
 
+#R = sympy.Symbol('R', real=True, positive=True)
+#W = sympy.symbols('W')
 
-RLC = [
-    ["V","E1",1,0],
-    ["R","R1",1,2],
-    ["L","L1",2,3],
-    ["C","C1",3,0,"U0"]
-]
+#Wilkinson_shema = [
+#    ["V", "Vg", 4, 0],
+#    ["R", "R1", 1, 4],
+#    ["R", "R2", 2, 0],
+#    ["R", "R3", 3, 0],
+#    ["R", "R4", 2, 3],
+#    ["T", "T1", [1,0], [2,0], [math.sqrt(2)*R, math.pi/2]],
+#    ["T", "T2", [1,0], [3,0], [math.sqrt(2)*R, math.pi/2]]
+#]
+#
+#system = Solution(Wilkinson_shema)
+#solution = system.symPyCAP(w=W, replacement = {"R1" : R, "R2" : R, "R3" : R, "R4" : 2*R})
+#
+#system.print_specific_solutions()
+#Zc = sympy.Symbol('Zc', real=True, positive=True)
+#tau = sympy.symbols('tau')
+#
+#
+#TLine_shema = [
+#    ["V", "Vg", 3, 0],
+#    ["R", "R1", 3, 1],
+#    ["T", "T1", [1,0], [2,0], [Zc,tau]],
+#    ["R", "R2", 2, 0, "C0"]
+#]
+#
+#
+#system = Solution(TLine_shema)
+#solution = system.symPyCAP(replacement = {"R1" : Zc, "R2" : Zc})
+#system.print_specific_solutions()
 
-circuit = Solution(RLC)
+T = [
+       
+       ["InductiveT","K1", [1, 0], [2, 0], ["L1", "L2", "L12"]],
+       ["V","Vg", 3, 0],
+       ["R","R1", 3, 1],
+       ["R","R2", 2, 4],
+       ["R","R3", 3, 4],       
+       ]
 
-E, R, C, L, w = symbols('E,R,C,L,w')
+L = sympy.Symbol('L', real=True, positive=True)
 
-solution = circuit.symPyCAP(omega = w, r = {"E1" : E, "R1" : R, "C1" : C, "L1" : L})
+system = Solution(T)
+system.symPyCAP()
 
-circuit.electric_circuit_specifications()
-circuit.print_solutions()
-circuit.print_specific_solutions()
+system.symPyCAP(replacement = {"L1" : L, "L2" : L,"L12" : L})
+system.print_specific_solutions()
+#system.print_solutions()
 
-print("With specific w:")
-
-solution = circuit.symPyCAP(omega = 1/L/C, r = {"E1" : E, "R1" : R, "C1" : C, "L1" : L})
-
-circuit.electric_circuit_specifications()
-circuit.print_solutions()
-circuit.print_specific_solutions()
